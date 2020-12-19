75bd583b16bfa4933d017265ca4020311602bd2d
Alexandre Soares <ale_santos.soares@hotmail.com>
2020-12-19 17:23:15 -0300

    chore: fix seed

--- database/seeders/20201219035505-order.js
+++ database/seeders/20201219035505-order.js
@@ -5,7 +5,7 @@ module.exports = {
   [{
     id: 'or_b8c2e248-4c84-4301-a03f-952ea72bcf94',
     statusId: 'st_dc093433-1fa0-409e-9c54-99488c3351fe',
-    pendingReview: true,
+    pendingReview: false,
     userId: 'us_a92a34bf-d0fc-4967-b78a-0ddf2955de4c',
     customerId: 'co_93ac00e9-dc56-457b-ada0-f719679c0a6b',
   },
