708e74166868e47f6759ecae0b904c334b58ce3b
Alexandre Soares <ale_santos.soares@hotmail.com>
2021-01-18 12:30:31 -0300

    chore: changes

--- database/seeders/20210105221507-product.js
+++ database/seeders/20210105221507-product.js
@@ -16,20 +16,6 @@ module.exports = {
     createdAt: new Date(),
     updatedAt: new Date(),
   },
-  {
-    id: 'pr_b0d19ed3-ac88-452d-ba6a-af62bed39747',
-    name: 'Mac book pro 13\' tela retina',
-    companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: 'pr_20d12ad4-5bbc-4269-a924-9dacfce259bd',
-    name: 'Iphone 11 256gb',
-    companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
 ], {}),
 
   down: (queryInterface) => queryInterface.bulkDelete('products', null, {}),
